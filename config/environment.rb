9e05a27e20414e5efe3b4997b37bca0389d0eed9
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-10 20:21:46 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,4 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-require 'sinatra/base'
-require 'sinatra/reloader'
-
 require_all 'app'
