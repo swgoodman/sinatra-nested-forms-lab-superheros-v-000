b23e4e6bcf1314ff5a8e0e27a1665adc43a4f58e
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-10 20:19:34 +0000

    automatically backed up by learn

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,12 +1,12 @@
 
 class Superhero
   attr_accessor :name, :power, :bio
+  @@superheroes = []
 
   def initialize(params)
     @name = params[:name]
     @power = params[:power]
     @bio = params[:bio]
   end
-
 
 end
